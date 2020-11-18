13ae3ad802c9dc9ded33bd9007a8ff67c9f34140
gxkjsfxy-lbw <74294581+gxkjsfxy-lbw@users.noreply.github.com>
2020-11-18 17:28:59 +0800

    Create 111803.函数.c

---
+++ 111803.函数.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int y;
+int x;
+scanf("%d",&x);
+if(x<1){
+printf("%d",y=x);
+}
+else if(x>=1&&x<10){
+printf("%d",y=2*x-1);
+}
+else {
+printf("%d",y=3*x-11);
+}
+}
