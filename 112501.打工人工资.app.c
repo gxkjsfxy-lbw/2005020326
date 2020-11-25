615dedb512576531c0c3a708de7c881206665aed
gxkjsfxy-lbw <74294581+gxkjsfxy-lbw@users.noreply.github.com>
2020-11-25 09:27:56 +0800

    Create 112501.打工人工资.app.c

---
+++ 112501.打工人工资.app.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n<=0){
+printf("花呗人\n");
+}
+ else if(n>=1&&n<=500){
+printf("赤贫打工人\n");
+}
+ else if(n>=501&&n<=1000){
+printf("贫困打工人\n");
+}
+ else if("n>=1001&&n<=50000")
+	 printf("人上人");
+else{
+printf("爹\n");
+}
+}
