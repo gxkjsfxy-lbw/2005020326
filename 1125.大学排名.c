95c9962e1d8979c4464f85e39844b88fdfbfb080
gxkjsfxy-lbw <74294581+gxkjsfxy-lbw@users.noreply.github.com>
2020-11-25 16:30:01 +0800

    Create 1125.大学排名.c

---
+++ 1125.大学排名.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n==1){
+printf("麻省理工大学");
+}
+else if(n==2){
+printf("斯坦福大学");
+}
+else if(n==3){
+printf("哈佛大学");
+}
+else if(n==4){
+printf("牛津大学");
+}
+else if(n==5){
+printf("加州理工学院");
+}
+else if(n==6){
+printf("苏黎世联邦工业大学");
+}
+else if(n==7){
+printf("剑桥大学");
+}
+else if(n==8){
+printf("伦敦大学学院");
+}
+else if(n==9){
+printf("帝国理工学院");
+}
+else if(n==10){
+printf("芝加哥大学");
+}
+else {
+printf("排名太后了,无法显示！！");
+}
+}
