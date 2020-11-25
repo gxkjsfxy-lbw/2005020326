00ce708a09d5818aad8b362083ce8afe7d2bf681
gxkjsfxy-lbw <74294581+gxkjsfxy-lbw@users.noreply.github.com>
2020-11-25 16:57:57 +0800

    Create 112503.延迟函数.c

---
+++ 112503.延迟函数.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<windows.h>
+void main()
+{
+printf("我突然\n");
+Sleep(3000);
+printf("发现\n");
+Sleep(3000);
+printf("你\n");
+Sleep(3000);
+printf("长得\n");
+Sleep(3000);
+printf("真的\n");
+Sleep(3000);
+printf("好\n");
+Sleep(3000);
+printf("牛逼！！\n");
+}
