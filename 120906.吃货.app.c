302e29c1744d65262687c930e7b53c88dc781158
gxkjsfxy-lbw <74294581+gxkjsfxy-lbw@users.noreply.github.com>
2020-12-10 20:23:23 +0800

    Update 120906.吃货.app.c

--- 120906.吃货.app.c
+++ 120906.吃货.app.c
@@ -2,22 +2,26 @@
 #include<string.h>
 void main()
 {
-	char n[]="节日";
+	char n1[]="春节";
+	char n2[]="元宵节";
+	char n3[]="端午节";
+	char n4[]="中秋节";
+	char n5[]="";
 	printf("吃货小程序，请问现在是什么节日？\n");
-	scanf("%s",&n);
-	if(n,"春节"){
-	printf("饺子");
+	scanf("%s",&n5);
+	if(strcmp(n1,n5)==0){
+		printf("饺子");
 	}
-	else if(n,"端午节"){
-	printf("粽子");
+	else if(strcmp(n2,n5)==0){
+		printf("元宵");
 	}
-	else if(n,"中秋节"){
-	printf("月饼");
+	else if(strcmp(n3,n5)==0){
+		printf("粽子");
 	}
-	else if(n,"元宵节"){
-	printf("元宵");
+	else if(strcmp(n4,n5)==0){
+		printf("月饼");
 	}
 	else{
-	printf("输入错误");
+		printf("输入错误");
 	}
 }
